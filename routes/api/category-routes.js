7c104e9679ff01ba71d17906f22e5a94938ecc2c
jochun <jkchun@protonmail.com>
2022-01-28 15:30:55 -1000

    update category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,14 +49,57 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      res.status(500).json(err);
+    });
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.upsert(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData[0]) {
+        res.status(404).json({ message: 'No Category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
+
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No Category found with this id' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
